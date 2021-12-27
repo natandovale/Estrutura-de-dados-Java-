7309e2cdda3a8303af6e5a46c7ce0c29dd0267ae
natanaeloliveira <natanaeloliveira@mobills.com.br>
2021-12-27 11:08:35 -0300

    Vetores de sequencia de valores

---
+++ VetoresSequenciaDeValores/src/ed/VetorTeste.java
@@ -0,0 +1,24 @@
+package ed;
+
+public class VetorTeste {
+	public static void main(String[] args) {
+		
+		 Aluno a1 = new Aluno("Joao");
+		 Aluno a2 = new Aluno("Paulo");
+		 Aluno a3 = new Aluno("Maria");
+		 Aluno a4 = new Aluno("Jose");
+		 
+		 Vetor lista = new Vetor();
+		 lista.adiciona(a1);
+		 lista.adiciona(a2);
+		 lista.adiciona(a3);
+		 lista.adiciona(1, a4);
+		 
+		 System.out.println(lista);
+		 
+		 //Aluno x = lista.pega(158);
+		 //System.out.println(x);
+		 
+		 
+	}
+}
