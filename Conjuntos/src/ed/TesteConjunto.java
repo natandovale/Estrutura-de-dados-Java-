150b43a0ad7b15e0ab8763ddc3ba034a9eeddfb1
natanaeloliveira <natanaeloliveira@mobills.com.br>
2022-01-12 09:17:31 -0300

    Conjuntos

---
+++ Conjuntos/src/ed/TesteConjunto.java
@@ -0,0 +1,18 @@
+package ed;
+
+public class TesteConjunto {
+
+	public static void main(String[] args) {
+		
+		Conjunto conj = new Conjunto();
+		
+		conj.adiciona("Azul");
+		conj.adiciona("Verde");
+		conj.adiciona("Amarelo");
+		conj.adiciona("Vermelho");
+		
+		
+		System.out.println(conj);
+	}
+
+}
